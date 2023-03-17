ea4abc849ebb48e0b30e596662d335c929de0dec
Anastasiia <guba.anastaysha@gmail.com>
2023-03-17 15:47:22 +0200

    added styled Box

---
+++ src/components/Box/Box.jsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+import {
+  space,
+  layout,
+  flexbox,
+  grid,
+  border,
+  color,
+  shadow,
+  typography,
+  background,
+  position,
+} from 'styled-system';
+
+export const Box = styled('div')(
+  space,
+  layout,
+  flexbox,
+  grid,
+  border,
+  color,
+  shadow,
+  typography,
+  background,
+  position
+);
