fe0dda7ca0c88406ff4feb203ad7233d767b4d18
Anastasiia <guba.anastaysha@gmail.com>
2023-03-17 16:33:21 +0200

    added conditions of routes

---
+++ src/components/Routes/RestrictedRoute.js
@@ -0,0 +1,12 @@
+// import { Navigate } from 'react-router-dom';
+// import { useAuth } from 'hooks';
+
+// /**
+//  * - If the route is restricted and the user is logged in, render a <Navigate> to redirectTo.
+//  * - Otherwise render the component
+//  */
+
+// export const RestrictedRoute = ({ component: Component, redirectTo = '/' }) => {
+//   const { isLoggedIn } = useAuth();
+//   return isLoggedIn ? <Navigate to={redirectTo} /> : Component;
+// };
