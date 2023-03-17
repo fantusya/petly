48a5a8580fd21ee3ea35d0b83b777dbfa5892389
Anastasiia <guba.anastaysha@gmail.com>
2023-03-17 16:33:01 +0200

    added routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,39 @@
-import { Box } from './Box/Box';
+// import { Route, Routes, Navigate } from 'react-router-dom';
+// import { RestrictedRoute, PrivateRoute } from './Routes/PrivateRoute';
+
 import { GlobalStyle } from 'globalStyles/globalStyle';
 import FontStyles from 'globalStyles/fontStyles';
 
-import Example from './Example';
-
 export const App = () => {
   return (
-    <Box as="section" pt={5}>
-      <Example />
+    <>
+      {/* <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="news" element={<NewsPage />} />
+          <Route path="notices/:categoryName" element={<NoticesPage />} />
+          <Route path="friends" element={<OurFriendsPage />} />
+          <Route
+            path="register"
+            element={
+              <RestrictedRoute
+                redirectTo="/user"
+                component={<RegisterPage />}
+              />
+            }
+          />
+          <Route
+            path="login"
+            element={
+              <RestrictedRoute redirectTo="/user" component={<LoginPage />} />
+            }
+          />
+          <Route path="*" element={<Navigate to="/" replace />}></Route>
+        </Route>
+      </Routes> */}
+
       <FontStyles />
       <GlobalStyle />
-    </Box>
+    </>
   );
 };
