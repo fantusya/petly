e511d792b8062d89e84ab143f89bb058a19f245a
Anastasiia <guba.anastaysha@gmail.com>
2023-03-17 15:48:05 +0200

    added example component

---
+++ src/components/Example/Example.jsx
@@ -0,0 +1,12 @@
+import { Container } from 'globalStyles/globalStyle';
+import { Title } from './Example.styled';
+
+export const Example = () => {
+  return (
+    <Container>
+      <Title>Here will be nice web-page:)</Title>
+    </Container>
+  );
+};
+
+export default Example;
