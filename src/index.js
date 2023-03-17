0ec8fac39d256711bacc45a7e31ed2a6421df24b
Anastasiia <guba.anastaysha@gmail.com>
2023-03-17 15:50:16 +0200

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+// import { Provider } from 'react-redux';
+// import { PersistGate } from 'redux-persist/integration/react';
+
+import { theme } from 'globalStyles/theme';
+
 import { App } from 'components/App';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      {/* <Provider store={store}> */}
+      {/* <PersistGate loading={null} persistor={persistor}> */}
+      <BrowserRouter>
+        {/* <BrowserRouter basename="/petly"> */}
+        <App />
+      </BrowserRouter>
+      {/* </PersistGate> */}
+      {/* </Provider> */}
+    </ThemeProvider>
   </React.StrictMode>
 );
