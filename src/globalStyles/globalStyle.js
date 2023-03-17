861db41a6e06ddac3663e8921a00712502a52991
Anastasiia <guba.anastaysha@gmail.com>
2023-03-17 15:49:22 +0200

    added global styles and container

---
+++ src/globalStyles/globalStyle.js
@@ -0,0 +1,43 @@
+import styled, { createGlobalStyle } from 'styled-components';
+import 'modern-normalize';
+
+export const GlobalStyle = createGlobalStyle`
+ body {
+    margin: 0;
+
+    font-family: 'Manrope', sans-serif;
+    font-size: ${p => p.theme.fontSizes[2]};
+    line-height: ${p => p.theme.lineHeights.text};
+    letter-spacing: ${p => p.theme.letterSpacing.text};
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+      monospace;
+  }
+
+  h1,h2,h3,h4,h5,h6,p {
+    margin: 0;
+  }
+
+  ul, ol {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+export const Container = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+
+  padding: 0 20px;
+
+  @media (min-width: ${p => p.theme.breakpoints[1]}) {
+    padding: 0 ${p => p.theme.space[5]}px;
+  }
+
+  @media (min-width: ${p => p.theme.breakpoints[2]}) {
+    padding: 0 ${p => p.theme.space[4]}px;
+  }
+`;
