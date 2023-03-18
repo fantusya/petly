8be60cb693104f67ae9443df22f71f19e37e3002
Dmitriy-Hrihor <dmytro.hryhor@gmail.com>
2023-03-18 23:05:05 +0200

    Предварительная разметка

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,5 +1,60 @@
+import { Formik } from 'formik';
+import * as yup from 'yup';
+import { useDispatch, useSelector } from 'react-redux';
+
+const schema = yup.object().shape({
+  email: yup.string().email('').required(),
+  password: yup
+    .string()
+    .min(4)
+    .max(12)
+    .matches(
+      /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/,
+      'Password is correct'
+    )
+    .required(),
+});
+
 export const LoginPage = () => {
-  return;
+  const dispatch = useDispatch();
+
+  const handleSubmit = (values, { resetForm }) => {
+    dispatch(logIn(values));
+    if (error !== null) {
+      resetForm();
+    }
+  };
+
+  return (
+    <div>
+      <Formik
+        initialValues={{ email: '', password: '' }}
+        onSubmit={handleSubmit}
+        validationSchema={schema}
+      >
+        <Form autoComplete="on">
+          <label htmlFor="email">Email</label>
+          <Field
+            type="email"
+            name="email"
+            placeholder="Please, enter your Email"
+            required
+          />
+          <Error name="email" component="div" />
+
+          <label htmlFor="password">Password</label>
+          <Field
+            type="password"
+            name="password"
+            placeholder="Please, enter your Password"
+            required
+          />
+          <ErrorMessage name="password" component="div" />
+          <button type="submit">Log in</button>
+        </Form>
+      </Formik>
+    </div>
+  );
 };
 
 export default LoginPage;
