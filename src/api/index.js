9df2413cf6c74d1b336d2697b425b5ee04825b49
Anastasiia <guba.anastaysha@gmail.com>
2023-03-19 19:45:24 +0200

    added page param

--- src/api/index.js
+++ src/api/index.js
@@ -3,12 +3,13 @@ import axios from 'axios';
 axios.defaults.baseURL = 'http://localhost:3030';
 // axios.defaults.baseURL = 'https://uninterested-hose-newt.cyclic.app';
 
-export const fetchData = async url => {
+export const fetchData = async (page, url) => {
   const {
-    data: { results, total },
-  } = await axios.get(url);
-  //   console.log('results', results);
+    data: { results, current_total, total },
+  } = await axios.get(url, { params: { page } });
+
+  console.log('results', results);
   //   console.log('total', total);
 
-  return { results, total };
+  return { results, current_total, total };
 };
