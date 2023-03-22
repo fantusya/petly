735f7c12cf9482cfc72763668d31788bf026f852
Anastasiia <guba.anastaysha@gmail.com>
2023-03-22 19:30:26 +0200

    added redux notices

---
+++ src/redux/notices/selectors.js
@@ -0,0 +1,28 @@
+// import { createSelector } from '@reduxjs/toolkit';
+
+export const selectNotices = state => state.notices.noticesItems;
+
+export const selectIsLoading = state => state.notices.isLoading;
+
+export const selectError = state => state.notices.error;
+
+export const selectFilter = state => state.filter;
+
+// export const selectVisibleContacts = state => {
+//   const contacts = selectContacts(state);
+//   const filterValue = selectFilter(state);
+
+//   return contacts.filter(contact =>
+//     contact.name.toLowerCase().includes(filterValue.toLowerCase())
+//   );
+// };
+
+// M E M O I Z E D
+// export const selectVisibleContacts = createSelector(
+//   [selectContacts, selectFilter],
+//   (contacts, filterValue) => {
+//     return contacts.filter(contact =>
+//       contact.name.toLowerCase().includes(filterValue.toLowerCase())
+//     );
+//   }
+// );
