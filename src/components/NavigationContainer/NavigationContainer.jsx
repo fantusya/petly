625092a032023622483a4c202ab79425d8ea1a0f
AlexRudnyk <alexander.rudnyk@ukr.net>
2023-03-23 11:28:17 +0200

    adds a link for google auth

--- src/components/NavigationContainer/NavigationContainer.jsx
+++ src/components/NavigationContainer/NavigationContainer.jsx
@@ -1,7 +1,14 @@
 import { NavContainer } from 'components/NavigationContainer/NavigationContainer.styled';
 
 const NavigationContainer = ({ children }) => {
-  return <NavContainer>{children}</NavContainer>;
+  return (
+    <>
+      <a href="https://uninterested-hose-newt.cyclic.app/api/users/google">
+        GOOGLE
+      </a>
+      <NavContainer>{children}</NavContainer>
+    </>
+  );
 };
 
 export default NavigationContainer;
