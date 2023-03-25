cbc1a03e8aa7111703876efd93cd2d93cf421343
Anastasiia <guba.anastaysha@gmail.com>
2023-03-25 19:30:15 +0200

    updated

--- src/components/NoticesCategoriesNav/NoticesCategoriesNav.jsx
+++ src/components/NoticesCategoriesNav/NoticesCategoriesNav.jsx
@@ -4,8 +4,8 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import AddNoticeModal from '../AddNoticeModal/AddNoticeModal';
 import {
-  NavBox,
-  NavBtnWrapper,
+  // NavBox,
+  // NavBtnWrapper,
   NavBtn,
   AddButton,
   NavUl,
@@ -28,29 +28,30 @@ export const NoticesCategoriesNav = () => {
   };
 
   return (
-    <NavUl>
-      <li>
-        <NavBtn to="sell">sell</NavBtn>
-      </li>
-      <li>
-        {' '}
-        <NavBtn to="lost-found">lost/found</NavBtn>
-      </li>
-      <li>
-        <NavBtn to="for-free">in good hands</NavBtn>
-      </li>
-      {isLoggedIn && (
-        <>
-          <li>
-            <NavBtn to="favorite">favorite ads</NavBtn>
-          </li>
-          <li>
-            <NavBtn to="own">my ads</NavBtn>
-          </li>
-        </>
-      )}
-    </NavUl>
-    /* <AddButton
+    <>
+      <NavUl>
+        <li>
+          <NavBtn to="sell">sell</NavBtn>
+        </li>
+        <li>
+          {' '}
+          <NavBtn to="lost-found">lost/found</NavBtn>
+        </li>
+        <li>
+          <NavBtn to="for-free">in good hands</NavBtn>
+        </li>
+        {isLoggedIn && (
+          <>
+            <li>
+              <NavBtn to="favorite">favorite ads</NavBtn>
+            </li>
+            <li>
+              <NavBtn to="own">my ads</NavBtn>
+            </li>
+          </>
+        )}
+      </NavUl>
+      <AddButton
         onClick={e => {
           e.preventDefault();
           isLoggedIn ? handleModalToggle() : navigate('/login');
@@ -62,7 +63,8 @@ export const NoticesCategoriesNav = () => {
           <AddPetBtnLink>Add pet</AddPetBtnLink>
         )}
       </AddButton>
-      {extended && <AddNoticeModal handleModalToggle={handleModalToggle} />} */
+      {extended && <AddNoticeModal handleModalToggle={handleModalToggle} />}
+    </>
   );
 };
 
