219ad4c8169b303f26a9ffc15ebaaca944cb4072
Anastasiia <guba.anastaysha@gmail.com>
2023-03-25 19:40:31 +0200

    updated

--- src/components/AddNoticeModal/AddNoticeModal.jsx
+++ src/components/AddNoticeModal/AddNoticeModal.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useDispatch } from 'react-redux';
+// import { useDispatch } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 import { useFormik } from 'formik';
