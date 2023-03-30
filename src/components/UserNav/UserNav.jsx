50c245c370043eeefaab405b362ef1e47889a657
Darya Shlyakhovaya <dshlyakhovaya1992@gmail.com>
2023-03-30 16:45:58 +0300

    fix account

--- src/components/UserNav/UserNav.jsx
+++ src/components/UserNav/UserNav.jsx
@@ -1,15 +1,12 @@
 import { StyledNavLink, AvatarIcon } from 'components/UserNav/UserNav.styled';
 import Avatar from '../../images/Avatar.png';
-import Media from 'react-media';
 
 export const UserNav = ({ setOpen }) => {
   return (
     <StyledNavLink
       to="/user"
       onClick={() => {
-        <Media queries={{ small: { maxWidth: 767.9 } }}>
-          {matches => matches.small && setOpen(false)}
-        </Media>;
+        setOpen(false);
       }}
     >
       <AvatarIcon src={Avatar} alt="avatar" />
